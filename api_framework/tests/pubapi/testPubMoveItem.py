e0a86642e86155f9e2469a3fa4ff25cec29ff05c
AnnaVVV <vergeles.Anna@gmail.com>
2015-06-12 17:09:35 -0700

    Added new method test_move_folder_enough_perms_as_power_user(self)

--- api_framework/tests/pubapi/testPubMoveItem.py
+++ api_framework/tests/pubapi/testPubMoveItem.py
@@ -40,3 +40,38 @@ class TestClass(TestCase):
         resp = self.calls.move_item(name=folder2, destination=folder1_path, parent_path=self.config.testpath)
         assert resp.status_code == httplib.NOT_FOUND
         assert resp.json['errorMessage'] == "Source path for move doesn't exist"
+
+    def test_move_folder_enough_perms_as_power_user(self):
+        # List permissions enough to move folder
+        perms = ['Editor', 'Full', 'Owner']
+        # Create 2 folders
+        folder1 = self.utils.random_name()
+        folder2 = self.utils.random_name()
+        folder2_path = '%s/%s' % (self.config.testpath, folder2)
+        folder1_path = '%s/%s' % (self.config.testpath, folder1)
+        folder = [folder1, folder2]
+        for perm1 in perms:
+
+            for perm2 in perms:
+                self.calls.create_folder(folder1)
+                resp = self.calls.set_perms(folder_path=folder_path1, users=self.config.puser, permission=perm)
+                #assert resp.status_code == httplib.OK
+                #resp = self.calls.list_perms(folder_path=folder_path1, users=self.config.puser)
+                #assert resp.status_code == httplib.OK
+                #assert resp.json['users'][0]['permission'] == perm
+                #assert resp.json['users'][0]['subject'] == self.config.puser
+                #assert len(resp.json['groups']) == 0
+                self.calls.create_folder(folder2)
+                resp = self.calls.set_perms(folder_path=folder_path2, users=self.config.puser, permission=perm2)
+                resp = self.calls.move_item(name=folder1, destination=folder2_path, username=self.config.puser)
+                assert resp.status_code == httplib.OK
+                assert resp.json == self.no_json
+                resp = self.calls.list_folders(folder_path=folder2_path)
+                assert resp.json['folders'][0]['name'] == folder1
+                resp = self.calls.list_folders(folder_path=folder1_path)
+                assert resp.status_code == httplib.NOT_FOUND
+                self.calls.delete_folder(folder2)
+
+
+
+
